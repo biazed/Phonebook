5d8d2d3b2eb953b45f691abc00456e154469f983
biazed <biazed.pwn@gmail.com>
2012-05-10 17:32:03 +0300

    First Commit

---
+++ Phonebook/Phonebook/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Phonebook
+{
+    partial class phonebook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.numberBox = new System.Windows.Forms.TextBox();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(105, 97);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 0;
+            this.searchBtn.Text = "Hae";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(71, 18);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(195, 20);
+            this.nameBox.TabIndex = 1;
+            // 
+            // numberBox
+            // 
+            this.numberBox.Location = new System.Drawing.Point(71, 55);
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(195, 20);
+            this.numberBox.TabIndex = 2;
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(35, 21);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(30, 13);
+            this.nameLbl.TabIndex = 3;
+            this.nameLbl.Text = "Nimi:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(18, 58);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(47, 13);
+            this.nameLabel.TabIndex = 4;
+            this.nameLabel.Text = "Numero:";
+            // 
+            // phonebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 138);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.searchBtn);
+            this.Name = "phonebook";
+            this.Text = "Puhelinluettelo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox numberBox;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
+
---
+++ Phonebook/Phonebook/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Phonebook
+{
+    public partial class phonebook : Form
+    {
+        public phonebook()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string name,number;
+            name = nameBox.Text;
+            number = numberBox.Text;
+            string names[] = name.Split(' ');
+            foreach (string word in names)
+	        {
+	          Console.WriteLine(word);
+	        }
+        }
+    }
+}
